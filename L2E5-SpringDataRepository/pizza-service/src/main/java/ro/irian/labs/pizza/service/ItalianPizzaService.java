ad28eacb1c545f84a40013a0fb4fde5110197fc0
gpapp <georgia.papp@irian.ro>
2025-05-14 10:31:04 +0300

    lab 2 ex 5 - spring data repositories

---
+++ L2E5-SpringDataRepository/pizza-service/src/main/java/ro/irian/labs/pizza/service/ItalianPizzaService.java
@@ -0,0 +1,36 @@
+package ro.irian.labs.pizza.service;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import ro.irian.labs.pizza.domain.Pizza;
+import ro.irian.labs.pizza.domain.PizzaJPARepository;
+import ro.irian.labs.pizza.service.api.PizzaService;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Service
+@Primary
+public class ItalianPizzaService implements PizzaService {
+
+    private final PizzaJPARepository pizzaRepository;
+
+    public ItalianPizzaService(PizzaJPARepository pizzaRepository) {
+        this.pizzaRepository = pizzaRepository;
+    }
+
+    @Override
+    public List<Pizza> getAllPizzas(){
+        return pizzaRepository.findAll();
+    }
+
+    @Override
+    public Pizza getPizzaById(Long id){
+        return pizzaRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public List<Pizza> getPizzaByNameContains(String nameContainsString){
+        return pizzaRepository.findAllByNameContainsIgnoreCase(nameContainsString);
+    }
+}
