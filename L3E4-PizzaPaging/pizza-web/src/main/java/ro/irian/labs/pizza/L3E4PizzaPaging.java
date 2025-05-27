3c5767b972195e386921fa8880450e61620ca1f7
gpapp <georgia.papp@irian.ro>
2025-05-27 18:04:07 +0300

    lab 3 ex 4

---
+++ L3E4-PizzaPaging/pizza-web/src/main/java/ro/irian/labs/pizza/L3E4PizzaPaging.java
@@ -0,0 +1,15 @@
+package ro.irian.labs.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = "ro.irian.labs")
+//@EntityScan(basePackages = "ro.irian.labs")
+public class L3E4PizzaPaging {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L3E4PizzaPaging.class, args);
+    }
+
+}
