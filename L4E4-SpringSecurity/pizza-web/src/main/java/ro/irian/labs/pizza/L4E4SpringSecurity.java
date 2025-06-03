1fd04f2a911844a9c2c60e6058f3ba994fe1daa5
gpapp <georgia.papp@irian.ro>
2025-06-03 17:59:57 +0300

    lab 4 ex 4 security

---
+++ L4E4-SpringSecurity/pizza-web/src/main/java/ro/irian/labs/pizza/L4E4SpringSecurity.java
@@ -0,0 +1,15 @@
+package ro.irian.labs.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = "ro.irian.labs")
+//@EntityScan(basePackages = "ro.irian.labs")
+public class L4E4SpringSecurity {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L4E4SpringSecurity.class, args);
+    }
+
+}
